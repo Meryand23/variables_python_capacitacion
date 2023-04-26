3d90163ee3eb7ad6055d1a8ce31b75185390860e
Meryand Santander <meryand.santander@gmail.com>
2023-04-26 17:07:24 -0400

    mi primer commit

---
+++ variables.py
@@ -0,0 +1,11 @@
+# declarando variables
+nombre ="Meryand"
+edad = 26
+valor_dolar = 810.20
+estudias = True
+
+# imprimiendo nuestras variables
+print(nombre)
+print(edad)
+print(valor_dolar)
+print(estudias)
